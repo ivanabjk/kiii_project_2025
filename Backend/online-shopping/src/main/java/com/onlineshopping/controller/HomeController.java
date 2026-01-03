3f0ff0f774deb4ca5da9b0c71fa617324bd8c1a7
ivanabjk <ivana.bojkovska.002@gmail.com>
2026-01-03 23:22:09 +0100

    Initial commit

---
+++ Backend/online-shopping/src/main/java/com/onlineshopping/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.onlineshopping.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Online Shopping API is running!";
+    }
+}
